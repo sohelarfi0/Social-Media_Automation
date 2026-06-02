8b31bdf96113365e78561cdbd5c7220cfa9eead5
sohel <sohelarfi0@gmail.com>
2026-06-02 14:49:58 +0530

    start working on  the scheduler UI page

--- client/src/pages/Scheduler.tsx
+++ client/src/pages/Scheduler.tsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { dummyPostsData } from "../assets/assets";
+
+
+
 
 const Scheduler = () => {
+
+  const [posts, setPosts] = useState<any[]>([]);
+  const [content, setContent] = useState("");
+  const [scheduledDate, setScheduledDate] = useState("");
+  const [scheduledTime, setScheduledTime] = useState("");
+  const [selectedPlatforms, setSelectedPlatforms] = useState<string[]>([]);
+  const [mediaFile, setMediaFile] = useState<File | null> (null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchPosts = async () => {
+    setPosts(dummyPostsData)
+  }
+
+  useEffect(()=>{
+   ( async ()=> await fetchPosts())();
+   const interval = setInterval(async ()=> await fetchPosts(), 1000);
+   return ()=> clearInterval(interval)
+
+  },[])
+
+
+
   return (
     <div>Scheduler</div>
   )
