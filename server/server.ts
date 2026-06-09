2295354ed9972a38d20d43a641c153c38c75923d
sohel <sohelarfi0@gmail.com>
2026-06-09 12:04:17 +0530

    started the backend of the project.

---
+++ server/server.ts
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+import connectDB from "./config/db.js";
+
+const app = express();
+
+//Database connection
+await connectDB();
+
+// Middleware
+app.use(cors())
+app.use(express.json());
+
+
+const port = process.env.PORT || 3000;
+
+app.get('/',(_req: Request, res: Response)=>{
+    res.send('Server is Live!');
+
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running at http://localhost:${port}`);
+
+});
