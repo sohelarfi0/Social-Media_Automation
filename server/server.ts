92373ffcfdae659759454fd995ecdc791d0eef37
sohel <sohelarfi0@gmail.com>
2026-06-10 11:31:04 +0530

    implemented the socialAuthController function

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import express, {NextFunction,Request, Response} from "express";
 import cors from 'cors';
 import connectDB from "./config/db.js";
 import authRouter from "./routes/authRoutes.js";
+import socialAuthRouter from "./routes/socialAuthRoutes.js";
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.get('/',(_req: Request, res: Response)=>{
 
 // api 
 app.use("/api/auth", authRouter);
+app.use("/api/oauth",socialAuthRouter);
 
 // Global error Handler
 app.use((err: any, _req: Request, res: Response, _next: NextFunction)=>{
