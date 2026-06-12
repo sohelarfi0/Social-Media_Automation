fa3dec03f8c6f1d5622ce54ccaa374ab44ef6029
sohel <sohelarfi0@gmail.com>
2026-06-13 04:23:40 +0530

    created the posting , creating content through ai

--- server/controllers/postController.ts
+++ server/controllers/postController.ts
@@ -1,12 +1,30 @@
 import { Response } from "express";
 import {AuthRequest} from "../middlewares/authMiddleware.js";
-
+import { GoogleGenAI } from "@google/genai";
 
 
 // Generate post
 // POST /api/posts/genrate
 export const genratePost = async (req: AuthRequest, res: Response): Promise<void> =>{
-    
+    try{
+        const {prompt, tone, genrateImage} = req.body;
+
+        const apiKey = process.env.GEMINI_API_KEY;
+        if(!apiKey){
+            res.status(400).json({message: "Gemini API is missing. Please add it to your server/.env file."})
+            return;
+        }
+        const ai = new GoogleGenAI({apiKey});
+
+        // Generate Text 
+        const textResponse = await ai.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: `Generate a social `,
+        });
+    }catch(error){
+
+
+    }
 
 }
 
